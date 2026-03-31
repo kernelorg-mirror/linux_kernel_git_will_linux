9fff05115a379d1cf089fb934390af93ce2579f6
Will Deacon <will@kernel.org>
2026-03-31 23:18:05 +0100

    scripts/gdb: arm64: Use TPIDRRO_EL0 instead of SP_EL0 for 'current'
    The current task pointer now lives in TPIDRRO_EL0 instead of SP_EL0, so
    update the gdb 'cpus.py' helper script with the new register allocation.
    
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- scripts/gdb/linux/cpus.py
+++ scripts/gdb/linux/cpus.py
@@ -185,7 +185,7 @@ def get_current_task(cpu):
             var_ptr = gdb.parse_and_eval("&current_task")
             return per_cpu(var_ptr, cpu).dereference()
     elif utils.is_target_arch("aarch64"):
-        current_task_addr = gdb.parse_and_eval("(unsigned long)$SP_EL0")
+        current_task_addr = gdb.parse_and_eval("(unsigned long)$TPIDRRO_EL0")
         if (current_task_addr >> 63) != 0:
             current_task = current_task_addr.cast(task_ptr_type)
             return current_task.dereference()
